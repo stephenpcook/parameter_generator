a70cb0d7e592d7dc23c75d9e1fe0e45bf0aa687d
Stephen P. Cook <s.cook4@exeter.ac.uk>
2022-11-02 06:32:11 +0000

    Parametrise tests

--- tests/test_enumprod.py
+++ tests/test_enumprod.py
@@ -1,27 +1,33 @@
 import pytest
 from rand_param import Prod_range
 
+@pytest.fixture
+def pr_3_2():
+    return Prod_range([3, 2])
 
-def test_prod_range_zeroth_item():
-    pr = Prod_range([3, 2])
-    assert (pr.iget(0) == (0, 0))
+
+def test_prod_range_zeroth_item(pr_3_2):
+    assert (pr_3_2.iget(0) == (0, 0))
 
 
-def test_prod_range_first_item():
-    pr = Prod_range([3, 2])
-    assert (pr.iget(1) == (0, 1))
+def test_prod_range_first_item(pr_3_2):
+    assert (pr_3_2.iget(1) == (0, 1))
+
+
+def test_prod_range_negative_index(pr_3_2):
+    assert (pr_3_2.iget(5) == pr_3_2.iget(-1))
 
 
 @pytest.mark.parametrize("i,expected", [(2, (1, 0)), (3, (1, 1)), (4, (2, 0)), (5, (2, 1))])
-def test_prod_items(i, expected):
-    pr = Prod_range([3, 2])
-    assert pr.iget(i) == expected
+def test_prod_items(pr_3_2, i, expected):
+    assert pr_3_2.iget(i) == expected
+
+
+def test_get_integer_too_high(pr_3_2):
+    with pytest.raises(IndexError):
+        pr_3_2.iget(6)
+
 
 @pytest.mark.skip
 def test_noninteger_inputs():
-    assert False
-
-
-@pytest.mark.skip
-def test_get_integer_too_high():
     assert False
