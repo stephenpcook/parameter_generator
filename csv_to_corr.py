8b4ad609258bee642220771238eb14397a675f80
Stephen P. Cook <s.cook4@exeter.ac.uk>
2022-11-23 14:11:36 +0000

    Make CLI for csv_to_corr

--- csv_to_corr.py
+++ csv_to_corr.py
@@ -1,17 +1,23 @@
+import argparse
 from typing import Optional
+import sys
 from pathlib import Path
 import pandas as pd
 
-CSV_IN = Path(__file__).parent / "../params.csv"
-HTML_OUT = Path(__file__).parent / "../params_corr.html"
+CSV_DEFAULT_IN = Path(__file__).parent / "../params.csv"
+HTML_DEFAULT_OUT = Path(__file__).parent / "../params_corr.html"
 
 
-def csv_to_corr_html(csv_in: Path, html_out: Path, ignore_cols: Optional[list[int]] = None):
-    df = pd.read_csv(csv_in, index_col=0)
+def csv_to_corr_html(csv_in: Path, html_out: Path, ignore_col: Optional[list[int]] = None):
+    df = pd.read_csv(csv_in)
     columns = list(df.columns)
-    if ignore_cols is not None:
-        for col in ignore_cols:
-            columns.remove(col)
+    if ignore_col is not None:
+        for col in ignore_col:
+            try:
+                columns.remove(col)
+            except ValueError as e:
+                raise ValueError('Value {:} not found in CSV columns.'.format(
+                    col)).with_traceback(e.__traceback__)
     corr = df[columns].corr()
     for ii in range(len(corr)):
         corr.iloc[ii, ii] = pd.NA
@@ -19,5 +25,25 @@ def csv_to_corr_html(csv_in: Path, html_out: Path, ignore_cols: Optional[list[in
     corr_styled.to_html(html_out)
 
 
+def cli_args():
+    parser = argparse.ArgumentParser(
+        prog='csv_to_corr_html',
+        description="Produce html correlation matrix from a CSV file."
+    )
+    parser.add_argument('csv_in', type=argparse.FileType('r'))
+
+    parser.add_argument(
+        'html_out', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
+        help="Path to write html file to - default to stdout.")
+
+    parser.add_argument(
+        '-i', '--ignore', action='append',
+        dest='ignore_col', metavar="COLUMN_NAME",
+        help='CSV column to ignore (repeat to ignore multiple columns).')
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    csv_to_corr_html(CSV_IN, HTML_OUT)
+    args = cli_args()
+    csv_to_corr_html(**vars(args))
